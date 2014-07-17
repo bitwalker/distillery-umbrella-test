aa860df4bb9c0952bd368d5d3834c648ed849087
Chris Molozian <chris@cmoz.me>
2014-07-17 20:15:48 +0100

    Use the latest style for defining a supervisor.

--- apps/master_app/lib/master_app.ex
+++ apps/master_app/lib/master_app.ex
@@ -4,6 +4,13 @@ defmodule MasterApp do
   # See http://elixir-lang.org/docs/stable/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    MasterApp.Supervisor.start_link
+    import Supervisor.Spec, warn: false
+
+    children = []
+    opts = [
+      strategy: :one_for_one,
+      name: MasterApp.Supervisor
+    ]
+    Supervisor.start_link(children, opts)
   end
 end
