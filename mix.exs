1b179c9ccdc997d6ff0a2379d06a6ddaf86ed428
Martin Langhoff <martin.langhoff@gmail.com>
2016-09-13 17:12:24 -0400

    Migrated to Distillery!

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule UmbrellaTest.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [{:exrm, github: "bitwalker/exrm", branch: "v1"},
+    [{:distillery, github: "bitwalker/distillery"},
      {:conform, github: "bitwalker/conform"}]
   end
 end
